3a2e8f8fc800ccdee01e5a618ed86ae1ae37d292
Uayeb Caballero <uayeb.caballero@gmail.com>
2020-11-05 14:39:22 -0600

    implementation of our first thunk

---
+++ src/components/courses/CourseList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+const CourseList = ({courses}) => (
+    <table>
+        <thead>
+            <tr>
+                <th/>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Category</th>
+            </tr>
+        </thead>
+        <tbody>
+            {courses.map( (course) => (
+                <tr key={course.id}>
+                    <td>
+                        <a
+                            className="btn btn-light"
+                            href={"http://pluralsight.com/courses/" + course.slug}
+                        >
+                            Watch
+                        </a>
+                    </td>
+                    <td>
+                        <Link to={"/course/" + course.slug}>{course.title}</Link>
+                    </td>
+                    <td>{course.authorName}</td>
+                    <td>{course.category}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+);
+
+CourseList.PropTypes = {
+    courses: PropTypes.array.isRequired
+};
+
+export default CourseList;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import courses from "./courseReducer";
+import authors from "./authorReducer"
+
+const rootReducer = combineReducers({
+  courses,authors
+});
+
+export default rootReducer;
